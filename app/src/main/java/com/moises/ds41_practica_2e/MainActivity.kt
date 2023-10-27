55895add7983231f34334f427f230ff1b96cee42
moisnoes0806 <castillomoises276@gmail.com>
2023-10-27 13:02:01 -0700

    Primer commit

---
+++ app/src/main/java/com/moises/ds41_practica_2e/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.moises.ds41_practica_2e
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
